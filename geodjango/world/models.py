5470d4e921ebe773c9bf3e6a8a37292571f94e11
William Tran <wtran29@gmail.com>
2019-06-21 21:29:00 -0700

    added geographic admin settings for admin.py and urls.py, defined WorldBorder objects by name

--- geodjango/world/models.py
+++ geodjango/world/models.py
@@ -15,6 +15,9 @@ class WorldBorder(models.Model):
     lat = models.FloatField()
     geom = models.MultiPolygonField(srid=4326)
 
+    def __str__(self):
+        return self.name
+
 
 # Auto-generated `LayerMapping` dictionary for WorldBorder model
 worldborders_mapping = {
