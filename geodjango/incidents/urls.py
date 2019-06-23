242186206200b8bdfaf363f1f564b2c3b2f9984f
William Tran <wtran29@gmail.com>
2019-06-23 15:55:54 -0700

    added incident dataset function to urls routing

--- geodjango/incidents/urls.py
+++ geodjango/incidents/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, county_datasets, incident_data
 
 app_name = 'incidents'
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('county_data/', county_datasets, name='county'),
+    path('incident_data/', incident_data, name='incident'),
 ]
