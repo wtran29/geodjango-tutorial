f11fd81682ad956ee1dfebfbcd3e249acf17f83f
William Tran <wtran29@gmail.com>
2019-06-22 18:32:19 -0700

    included admin capabilities to incidents model, added county model

--- geodjango/incidents/admin.py
+++ geodjango/incidents/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.gis import admin
-from .models import Incident
+from .models import Incident, County
 from leaflet.admin import LeafletGeoAdmin
 
 
@@ -7,4 +7,11 @@ class IncidentAdmin(LeafletGeoAdmin):
     list_display = ('name', 'location')
 
 
+class CountyAdmin(admin.GeoModelAdmin):
+    list_display = ('name', 'geoid')
+
+
+admin.site.register(County, CountyAdmin)
 admin.site.register(Incident, IncidentAdmin)
+
+
--- geodjango/incidents/models.py
+++ geodjango/incidents/models.py
@@ -10,3 +10,29 @@ class Incident(models.Model):
     def __str__(self):
         return self.name
 
+
+class County(models.Model):
+    statefp = models.CharField(max_length=2)
+    countyfp = models.CharField(max_length=3)
+    countyns = models.CharField(max_length=8)
+    geoid = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
+    namelsad = models.CharField(max_length=100)
+    lsad = models.CharField(max_length=2)
+    classfp = models.CharField(max_length=2)
+    mtfcc = models.CharField(max_length=5)
+    csafp = models.CharField(max_length=3)
+    cbsafp = models.CharField(max_length=5)
+    metdivfp = models.CharField(max_length=5)
+    funcstat = models.CharField(max_length=1)
+    aland = models.BigIntegerField()
+    awater = models.BigIntegerField()
+    intptlat = models.CharField(max_length=11)
+    intptlon = models.CharField(max_length=12)
+    geom = models.MultiPolygonField(srid=4326)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'counties'
