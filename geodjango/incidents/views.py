3bff120fc632649d43a61fb5ca5207c13bd44988
William Tran <wtran29@gmail.com>
2019-06-23 15:56:58 -0700

    created function to pick up all incidents

--- geodjango/incidents/views.py
+++ geodjango/incidents/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView, DetailView
+from django.core.serializers import serialize
+
+from .models import County, Incident
 
 
 class HomePageView(TemplateView):
     template_name = 'incidents/index.html'
 
 
+def county_datasets(request):
+    counties = serialize('geojson', County.objects.filter(geoid='06037'))
+    return HttpResponse(counties, content_type='json')
+
+
+def incident_data(request):
+    points = serialize('geojson', Incident.objects.all())
+    return HttpResponse(points, content_type='json')
